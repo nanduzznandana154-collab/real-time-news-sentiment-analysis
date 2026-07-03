3801dc9d689488a65a20e79220143aa5e5cd71de
nanduzznandana154-collab <nanduzznandana154@gmail.com>
2026-07-03 14:44:21 +0530

    Add AWS RDS database connection using environment variables

---
+++ lambda/rds_test.py
@@ -0,0 +1,17 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST1"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD1"),
+    port=os.getenv("DB_PORT")
+)
+
+print("Connected to AWS RDS!")
+
+conn.close()

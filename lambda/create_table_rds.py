f6c0cee574132d4c15e095431943d72013acb189
nanduzznandana154-collab <nanduzznandana154@gmail.com>
2026-07-06 11:54:27 +0530

    Connect to AWS RDS PostgreSQL database

--- lambda/create_table_rds.py
+++ lambda/create_table_rds.py
@@ -3,3 +3,11 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST1"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD1"),
+    port=os.getenv("DB_PORT")
+)

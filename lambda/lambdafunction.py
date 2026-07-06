0d69e886793ca9109153641c914c429618d2319b
nanduzznandana154-collab <nanduzznandana154@gmail.com>
2026-07-06 14:21:44 +0530

    Fetch latest news from NewsData API

--- lambda/lambdafunction.py
+++ lambda/lambdafunction.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def lambda_handler(event, context):
-    
+
     API_KEY = os.getenv("API_KEY")
     
+    url = f"https://newsdata.io/api/1/latest?apikey=pub_4d3a5600b39c4069b1155376328c26f2&language=en"
+
+    data = requests.get(url).json()

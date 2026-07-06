5baa06f18b2467c678582f2dafa34ae853ab4230
nanduzznandana154-collab <nanduzznandana154@gmail.com>
2026-07-06 12:42:45 +0530

    Connect to AWS RDS PostgreSQL database

--- lambda/save_to_rds.py
+++ lambda/save_to_rds.py
@@ -7,3 +7,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
+
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST1"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD1"),
+    port=os.getenv("DB_PORT")
+)
